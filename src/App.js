9419664d215cf780cd90a91095f509471f8fed2e
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-23 13:52:57 -0300

    Enhancement - Added modal to show when the API call ends

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 
 import AppBar from 'material-ui/AppBar';
 import AutoComplete from 'material-ui/AutoComplete';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -26,7 +28,7 @@ const paperStyles = {
   };
 
 class App extends Component {
-  state = { cities: [], city: '', step: 'First', firstName: '', lastName: '', phone: '', email: '' };
+  state = { cities: [], city: '', step: 'First', firstName: '', lastName: '', phone: '', email: '', showSuccessDialog: false };
 
   handleUpdateInput = searchText => {
     if (!searchText) return;
@@ -57,29 +59,38 @@ class App extends Component {
   updateEmail = event => this.setState({ email: event.target.value });
 
   sendUserData = () => {
-    const {city, firstName, lastName, phone, email} = this.state;
+    const { city, firstName, lastName, phone, email } = this.state;
+
     fetch('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({city, firstName, lastName, phone, email})
-    })
-  }
+      body: JSON.stringify({ city, firstName, lastName, phone, email })
+    }).then(() => {
+      this.setState({ showSuccessDialog: true });
+    });
+  };
 
-  isFormValid () {
-    const {city, cities, firstName, lastName, phone, email} = this.state;
+  isFormValid() {
+    const { firstName, lastName, phone, email } = this.state;
     return this.isCityValid() && firstName && lastName && phone && email;
   }
 
-  isCityValid () {
-    const {city, cities} = this.state;
+  isCityValid() {
+    const { city, cities } = this.state;
     return city && cities.indexOf(city) !== -1;
   }
 
+  handleClose = () => {
+    this.setState({ showSuccessDialog: false });
+  };
+
   render() {
-    const { cities, city, step, firstName, lastName, phone, email } = this.state;
+    const { cities, city, step, firstName, lastName, phone, email, showSuccessDialog } = this.state;
+
+    const actions = [<FlatButton label="Ok" primary={true} onClick={this.handleClose} />];
     return (
       <MuiThemeProvider>
         <Paper style={paperStyles}>
@@ -93,7 +104,13 @@ class App extends Component {
                   style={{ display: 'block', margin: 15 }}
                   searchText={city}
                 />
-                <RaisedButton label="Next" primary={true} style={{ display: 'block', margin: 15 }} onClick={this.nextStep} disabled={!this.isCityValid()}/>
+                <RaisedButton
+                  label="Next"
+                  primary={true}
+                  style={{ display: 'block', margin: 15 }}
+                  onClick={this.nextStep}
+                  disabled={!this.isCityValid()}
+                />
               </form>
             : <form style={containerStyles}>
                 <TextField hintText="First Name" onChange={this.updateFirstName} value={firstName} />
@@ -101,8 +118,17 @@ class App extends Component {
                 <TextField hintText="Phone" onChange={this.updatePhone} value={phone} />
                 <TextField hintText="E-mail" onChange={this.updateEmail} value={email} />
                 <RaisedButton label="Back" primary={false} style={{ display: 'block', margin: 15 }} onClick={this.backStep} />
-                <RaisedButton label="Confirm" primary={true} style={{ display: 'block', margin: 15 }} onClick={this.sendUserData} disabled={!this.isFormValid()} />
+                <RaisedButton
+                  label="Confirm"
+                  primary={true}
+                  style={{ display: 'block', margin: 15 }}
+                  onClick={this.sendUserData}
+                  disabled={!this.isFormValid()}
+                />
               </form>}
+          <Dialog actions={actions} modal={false} open={showSuccessDialog} onRequestClose={this.handleClose}>
+            Your data was successfully sent
+          </Dialog>
         </Paper>
       </MuiThemeProvider>
     );
