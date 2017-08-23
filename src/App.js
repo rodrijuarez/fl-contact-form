3d40253948908ac2642b62da2682e1e2b18d58f2
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-23 11:44:41 -0300

    Enhancement - UI Improvement
     - Usage of basic Material UI components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
+import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+
+const paperStyles = {
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  right: '0',
+  bottom: '0',
+  backgroundColor: 'transparent'
+};
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <h1>FinLeap Contact Form</h1>
+        <Paper>
+          <AppBar title="FinLeap Contact Form" showMenuIconButton={false} />
+        </Paper>
       </MuiThemeProvider>
     );
   }
